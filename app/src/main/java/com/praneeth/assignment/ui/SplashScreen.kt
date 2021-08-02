154a742d17e24aae7f77fbc03dfbdcb72db8242f
DineshReddyP <praneeth.t96@gmail.com>
2021-08-02 15:19:15 +0530

    splash screen added

---
+++ app/src/main/java/com/praneeth/assignment/ui/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.praneeth.assignment.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+import com.praneeth.assignment.R
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+       Handler().postDelayed({
+
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+    }
+}
